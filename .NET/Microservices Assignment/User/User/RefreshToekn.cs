c70a56ca57b809bb2b1df576e60795843b2ae56f
strenuousnerd8 <zakkiwat@gmail.com>
2022-12-22 17:18:29 +0530

    Latest

--- .NET/Microservices Assignment/User/User/RefreshToekn.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace User
-{
-    public class RefreshToekn
-    {
-        public string Token { get; set; } = string.Empty;
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime Expires { get; set; }
-    }
-}
--- .NET/Microservices Assignment/User/User/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace User
-{
-    public class UserDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
